9ddd8f11ab8440d922c76bec271499bd21fdea55
Jameser22 <ikultin@gmail.com>
2026-03-15 14:03:20 +0300

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,1 +1,15 @@
-handlers/start.py
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from keyboards.main_menu import intro_kb
+
+router = Router()
+
+@router.message(Command("start"))
+@router.message(Command("krossmeup"))
+async def cmd_krossmeup(message: Message):
+    await message.answer(
+        "«Логика — это анатомия мышления.»\n— Джон Локк",
+        reply_markup=intro_kb()
+    )
