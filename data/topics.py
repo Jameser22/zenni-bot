668af65a18d43b25af9efd077fc939fb4d82e76d
Jameser22 <ikultin@gmail.com>
2026-03-15 14:44:23 +0300

    Update topics.py

--- data/topics.py
+++ data/topics.py
@@ -20,3 +20,11 @@ CATEGORIES = {
         ],
     },
 }
+
+
+def get_topic_title(topic_id: str) -> str:
+    for category in CATEGORIES.values():
+        for title, current_topic_id in category["topics"]:
+            if current_topic_id == topic_id:
+                return title
+    return topic_id
